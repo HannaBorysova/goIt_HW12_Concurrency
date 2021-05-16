e41ac62db3932b99660ed57b536c3dfaf8fc0b01
Анна <haborysova@gmail.com>
2021-05-16 11:34:27 +0300

    HW12_taskOne

--- src/main/java/com/borysova/taskOne/H2O.java
+++ src/main/java/com/borysova/taskOne/H2O.java
@@ -2,7 +2,7 @@ package main.java.com.borysova.taskOne;
 
 import java.util.concurrent.Semaphore;
 
-public class H2Omol {
+public class H2O {
     private Semaphore semaphoreH = new Semaphore(2);
     private Semaphore semaphoreO = new Semaphore(1);
     private Semaphore semaphore1 = new Semaphore(0);
@@ -24,3 +24,32 @@ public class H2Omol {
         semaphoreO.release();
     }
 }
+
+class H2OTest {
+
+    public static void main(String[] args) {
+        H2O h2o = new H2O();
+        int n = 2;
+
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 2*n; i++) {
+                    h2o.hydrogen(() -> System.out.print("H"));
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < n; i++) {
+                    h2o.oxygen(() -> System.out.print("O"));
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+}
+
